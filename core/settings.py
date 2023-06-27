812fcd9012a506aabf2ab1877e84132bd1a98f81
GonzaMaldonado <nahuel.maldonado.gonzalo@gmail.com>
2023-06-26 22:43:37 -0300

    fix: cambiando confi cloudinary

--- core/settings.py
+++ core/settings.py
@@ -141,6 +141,7 @@ STATIC_URL = 'static/'
 
 if not DEBUG:
     STATIC_ROOT= os.path.join(BASE_DIR, 'staticfiles')
+    STATICFILES_STORAGE = 'cloudinary_storage.storage.StaticHashedCloudinaryStorage'
     #STATICFILES_STORAGE= 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 #Ubicacion de los archivos estaticos
 STATIC_ROOT = BASE_DIR / 'static'
